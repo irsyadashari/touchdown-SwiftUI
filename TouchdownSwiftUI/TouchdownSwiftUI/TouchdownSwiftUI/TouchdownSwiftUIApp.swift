73851025a9da771c464c504a54e55b59b03ad02f
Irsyad Ashari <muh.ashari@gojek.com>
2024-11-06 22:59:31 +0700

    init

---
+++ TouchdownSwiftUI/TouchdownSwiftUI/TouchdownSwiftUI/TouchdownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownSwiftUIApp.swift
+//  TouchdownSwiftUI
+//
+//  Created by Muh Irsyad Ashari on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
